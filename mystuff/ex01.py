8305e7c8b348ce6d5e278533baff262387b3574d
WangQ <yudianzhiyu@gmail.com>
2015-08-29 19:37:13 +0800

    delete file

--- mystuff/ex01.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-print "Hello World:"
-print "hello Again"
-print "I like typing this"
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'i "said" do not touch this.'
-
--- mystuff/ex13.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from sys import argv
-
-script,first ,second,third = argv
-
-print "the script is called:", script
-print "the first variable is : ", first
-print "you second variable is :",second
-print "you third variable is :",third
